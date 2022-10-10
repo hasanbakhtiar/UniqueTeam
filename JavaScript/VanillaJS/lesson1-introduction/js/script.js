a084e90dc013701d7074115cfe5fa3f04edaa706
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-10-10 12:39:07 +0400

    js enter

---
+++ JavaScript/VanillaJS/lesson1-introduction/js/script.js
@@ -0,0 +1,19 @@
+//one line comment
+/*
+more 
+line 
+comment
+
+*/
+// debugger
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+
+// document.write('Hello World - External file');
+
+// alert('hello');
+// window.alert('hello window');
---
+++ JavaScript/VanillaJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,39 @@
+// Data Types
+// primitive data types
+
+// string
+// var infoOne = "Hello ";
+// var infoTwo = "JavaScript";
+// console.log(infoOne+infoTwo);
+
+// number
+// var infoOne = 5;
+// var infoTwo = "15";
+// console.log(infoOne+infoTwo);
+
+// boolean
+// true - 1
+// false - 0
+// if (false) {
+//     console.log('this is successfull');
+// }else{
+//     console.log('this is wrong');
+// }
+
+
+// composite data types
+// array
+// var infoArr = [1,2,true,'Hello'];
+// console.log(infoArr);
+// object
+// var infoCar = {
+//     model: "BMW M5",
+//     year:2022
+// }
+// console.log(infoCar);
+
+// special data types
+// undefined
+// console.log(undefined);
+// null
+// console.log(null);

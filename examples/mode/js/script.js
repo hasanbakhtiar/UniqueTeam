7a44bb42cd916dc46b391b961eb1f28cf451cc91
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-03 13:00:15 +0400

    mode

--- examples/mode/js/script.js
+++ examples/mode/js/script.js
@@ -1,6 +1,8 @@
 const modeBtn = document.querySelector('.mode-btn');
 const nav = document.querySelector('nav');
-
+const input = document.querySelector('input');
+const text = document.querySelector('h1');
+const mode = document.querySelector('#mode');
 modeBtn.onclick=(e)=>{
     e.preventDefault();
     if (nav.className === "navbar navbar-expand-lg bg-light navbar-light") {
@@ -8,11 +10,16 @@ modeBtn.onclick=(e)=>{
         nav.className = "navbar navbar-expand-lg bg-dark navbar-dark";
         modeBtn.className = "btn btn-outline-light mode-btn";
         document.body.className = 'bg-dark';
-
+        text.innerHTML = `<h1 class="text-light">${input.value}</h1>`;
+        mode.className = "dark-mode";
     }else{
         modeBtn.innerHTML = "🌙"
         nav.className = "navbar navbar-expand-lg bg-light navbar-light";
         modeBtn.className = "btn btn-outline-dark mode-btn";
         document.body.className = 'bg-light';
+        text.innerHTML = `<h1 class="text-dark">${input.value}</h1>`;
+        mode.className = "light-mode";
+
+
     }
 }

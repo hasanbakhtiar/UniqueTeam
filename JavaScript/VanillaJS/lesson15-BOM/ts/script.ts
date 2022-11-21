2be555d83d30dfb67d954096edc1cfed612528ae
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-21 12:56:19 +0400

    BOM

---
+++ JavaScript/VanillaJS/lesson15-BOM/ts/script.ts
@@ -0,0 +1,71 @@
+// const infoText:any = document.querySelector<HTMLParagraphElement>('p');
+// const btn:any = document.querySelectorAll<HTMLButtonElement>('button')[0];
+// const closeWind:any = document.querySelectorAll<HTMLButtonElement>('button')[0];
+// let myWindow;
+// const infoFunc = () =>{
+    // infoText.innerHTML = screen.width+"px";
+    // infoText.innerHTML = screen.height+"px";
+    // infoText.innerHTML = screen.availWidth+"px";
+    // infoText.innerHTML = screen.availHeight+"px";
+
+    // infoText.innerHTML = window.innerHeight+"px";
+    // infoText.innerHTML = window.innerWidth+"px";
+    // window.open('https://google.com',"","width=500,height=400");
+    // myWindow = window.open("", "MsgWindow", "width=500,height=400");
+    // myWindow.document.write("<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>");
+
+    // infoText.innerHTML = screen.pixelDepth;
+    // infoText.innerHTML = window.location.hostname;
+    // infoText.innerHTML = window.location.host;
+    // infoText.innerHTML = window.location.port;
+    // infoText.innerHTML = window.location.protocol;
+    // infoText.innerHTML = window.location.href;
+    // infoText.innerHTML = window.location.pathname;
+    // infoText.innerHTML = window.location.assign('https://google.com');
+    // infoText.innerHTML = navigator.cookieEnabled;
+    // infoText.innerHTML = navigator.appName;
+    // infoText.innerHTML = prompt();
+    // if (confirm("Press a button!")) {
+    //     infoText.innerHTML = "You pressed OK!";
+    //   } else {
+    //     infoText.innerHTML = "You pressed Cancel!";
+    //   }
+//     document.write("hello");
+    
+// } 
+// 1000ms = 1s
+// setInterval(infoFunc,50);
+// let data = setInterval(()=>{
+// infoText.innerHTML = new Date();
+// },1000);
+// closeWind.onclick=()=>{
+//     clearInterval(data);
+// }
+// closeWind.onclick=()=>{
+//     myWindow.resizeTo(1500,400);
+// }
+// btn.onclick=infoFunc;
+// ["af", "sq", "ar-SA", "ar-IQ", "ar-EG", "ar-LY", "ar-DZ", "ar-MA", "ar-TN", "ar-OM",
+//  "ar-YE", "ar-SY", "ar-JO", "ar-LB", "ar-KW", "ar-AE", "ar-BH", "ar-QA", "eu", "bg",
+//  "be", "ca", "zh-TW", "zh-CN", "zh-HK", "zh-SG", "hr", "cs", "da", "nl", "nl-BE", "en",
+//  "en-US", "en-EG", "en-AU", "en-GB", "en-CA", "en-NZ", "en-IE", "en-ZA", "en-JM",
+//  "en-BZ", "en-TT", "et", "fo", "fa", "fi", "fr", "fr-BE", "fr-CA", "fr-CH", "fr-LU",
+//  "gd", "gd-IE", "de", "de-CH", "de-AT", "de-LU", "de-LI", "el", "he", "hi", "hu", 
+//  "is", "id", "it", "it-CH", "ja", "ko", "lv", "lt", "mk", "mt", "no", "pl",
+//  "pt-BR", "pt", "rm", "ro", "ro-MO", "ru", "ru-MI", "sz", "sr", "sk", "sl", "sb",
+//  "es", "es-AR", "es-GT", "es-CR", "es-PA", "es-DO", "es-MX", "es-VE", "es-CO", 
+//  "es-PE", "es-EC", "es-CL", "es-UY", "es-PY", "es-BO", "es-SV", "es-HN", "es-NI", 
+//  "es-PR", "sx", "sv", "sv-FI", "th", "ts", "tn", "tr", "uk", "ur", "ve", "vi", "xh",
+//  "ji", "zu"];
+
+if (navigator.language === 'en-GB') {
+    
+}
+
+// setTimeout(()=>{
+//     document.querySelector('#con').className ='d-block';
+//     document.querySelector('#preloader').className ='d-none';
+// },2000);
+
+
+// https://www.w3schools.com/js/js_dates.asp

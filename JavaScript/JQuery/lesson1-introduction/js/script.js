40a0d1a0477e2c2218ec56e6fc08baabf422b5dd
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-24 13:01:36 +0400

    jquery end

---
+++ JavaScript/JQuery/lesson1-introduction/js/script.js
@@ -0,0 +1,25 @@
+// info(function(){
+//     info('ne ile').nece(function(){
+//         info('neyi').edecem();
+//     })
+// })
+var info = $.noConflict();
+info(document).ready(function(){
+    info('button').click(function(){
+        // info("*").hide(1000);
+        // info(".box").fadeIn(1000);
+        // info(".box").fadeToggle(1000);
+        // info(".box").slideDown(1000);
+        // info(".box").animate({
+        //     left: '250px',
+        //     opacity: '0.5',
+        //     height: '150px',
+        //     width: '+=150px'
+        //   },()=>{alert('hello')});
+        
+        // info('.box').css({"background-color":"green",'color':"white"});
+        // alert(info('h1').text());
+        // info('h1').text( info('input').val());
+        //  info('input').val('ajhjgasdka');
+    })
+})
